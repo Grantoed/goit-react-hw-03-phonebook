1fed98501a28cba18f074ff812a8eb7241ffd4be
Ruslan Babaiev <ruslan.nadirovich@gmail.com>
2022-10-24 16:19:10 +0300

    fixed local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,9 @@ export class App extends Component {
 
   componentDidMount() {
     const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
-    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   componentDidUpdate(_, prevState) {
